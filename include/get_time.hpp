9f4fbe19b69e0a5c2c12dd0948decf05948856f7
M. Galib Uludag <m231@tnctr.com>
2023-04-16 01:31:00 +0200

    Update get_time.hpp

--- include/get_time.hpp
+++ include/get_time.hpp
@@ -7,18 +7,18 @@ inline auto parse_integer(std::string_view str, uint32_t len, uint32_t& next,
                           uint32_t begin_offset = 0) -> int32_t {
     int32_t result{0};
     if (str.size() < len + next)
-        throw std::invalid_argument("value is not convertible!");
+        throw std::invalid_argument("value is not convertible!1");
     for (auto it{str.begin() + next + begin_offset};
          it != str.begin() + len + next; ++it) {
         if (!std::isdigit(*it))
-            throw std::invalid_argument("value is not convertible!");
+            throw std::invalid_argument("value is not convertible!2");
     }
     auto error = std::from_chars(str.begin() + next + begin_offset,
                                  str.begin() + len + next, result);
 
     next = ++len + next;
     if (error.ec != std::errc())
-        throw std::invalid_argument("value is not convertible!");
+        throw std::invalid_argument("value is not convertible!3");
     return result;
 }
 
@@ -54,27 +54,27 @@ std::enable_if_t<std::is_same_v<std::tm, T>, T> constexpr get_time(
 
     T tm{};
 
-    // auto change_date = [&tm](int32_t offset) {
-    //     const auto minute = offset % 100;
-    //     const auto hour = offset / 100;
-    //     if (offset < 0) {
-    //         tm.tm_min + minute < 0 ? tm.tm_hour - 1 < 0 ? --tm.tm_mday,
-    //             tm.tm_hour = 23 - tm.tm_hour : --tm.tm_hour : 0;
-    //         tm.tm_min -= minute;
-    //         tm.tm_min %= 60;
+    auto change_date = [&tm](int32_t offset) {
+        const auto minute = offset % 100;
+        const auto hour = offset / 100;
+        if (offset < 0) {
+            tm.tm_min + minute < 0 ? tm.tm_hour - 1 < 0 ? --tm.tm_mday,
+                tm.tm_hour = 23 - tm.tm_hour : --tm.tm_hour : 0;
+            tm.tm_min -= minute;
+            tm.tm_min %= 60;
 
-    //         tm.tm_hour + hour < 0      ? --tm.tm_mday,
-    //             tm.tm_hour = 23 + hour : tm.tm_hour += hour;
-    //     } else {
-    //         tm.tm_min + minute > 59 ? tm.tm_hour + 1 > 23 ? ++tm.tm_mday,
-    //             ++tm.tm_hour %= 24 : ++tm.tm_hour : 0;
-    //         tm.tm_min += minute;
-    //         tm.tm_min %= 60;
+            tm.tm_hour + hour < 0      ? --tm.tm_mday,
+                tm.tm_hour = 23 + hour : tm.tm_hour += hour;
+        } else {
+            tm.tm_min + minute > 59 ? tm.tm_hour + 1 > 23 ? ++tm.tm_mday,
+                ++tm.tm_hour %= 24 : ++tm.tm_hour : 0;
+            tm.tm_min += minute;
+            tm.tm_min %= 60;
 
-    //         tm.tm_hour + hour > 23                    ? ++tm.tm_mday,
-    //             tm.tm_hour = (tm.tm_hour + hour) % 24 : tm.tm_hour += hour;
-    //     }
-    // };
+            tm.tm_hour + hour > 23                    ? ++tm.tm_mday,
+                tm.tm_hour = (tm.tm_hour + hour) % 24 : tm.tm_hour += hour;
+        }
+    };
 
     uint32_t next{0};
 
@@ -122,27 +122,39 @@ std::enable_if_t<std::is_same_v<std::tm, T>, T> constexpr get_time(
                         check_range(tm.tm_min, 0, 59);
                         check_range(tm.tm_sec, 0, 59);
                     } break;
-                    // case 'z': {
-                    //     char sign{};
-                    //     auto diff{0};
-                    //     for (auto j{next - 1}; j < date_str.size(); ++j) {
-                    //         if (date_str[j] == '-' || date_str[j] == '+') {
-                    //             sign = date_str[j];
-                    //             diff = j - next + 1;
-                    //             break;
-                    //         }
-                    //     }
-                    //     auto offset = parse_integer(
-                    //         date_str, date_str.size() - next, next, diff);
-                    //     switch (sign) {
-                    //         case '+':
-                    //             change_date(offset);
-                    //             break;
-                    //         case '-':
-                    //             change_date(offset * -1);
-                    //             break;
-                    //     }
-                    // } break;
+                    case 'z': {
+                        char sign{};
+                        auto diff{0};
+                        for (auto j{next - 1}; j < date_str.size(); ++j) {
+                            if (date_str[j] == '-' || date_str[j] == '+') {
+                                sign = date_str[j];
+                                diff = j - next + 1;
+                                break;
+                            }
+                        }
+                        auto hour_offset_str = std::string_view{date_str.begin() + next + diff,
+                                 date_str.end()};
+                        auto pos = hour_offset_str.find(':');
+                        auto offset{0};
+                        if(pos != std::string::npos){
+                            next = 0;
+                            auto hour_offset = parse_integer(hour_offset_str, 2, next);
+                            auto min_offset = parse_integer(hour_offset_str, 2, next);
+                            offset = hour_offset * 100 + min_offset;
+                        }
+                        else{
+                            offset = parse_integer(
+                            date_str, date_str.size() - next, next, diff);
+                        }
+                        switch (sign) {
+                            case '+':
+                                change_date(offset * -1);
+                                break;
+                            case '-':
+                                change_date(offset);
+                                break;
+                        }
+                    } break;
                 }
             } break;
             case ' ':
