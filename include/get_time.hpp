e29071410ee2afcb2d160c520e6407c720ea9fd0
M. Galib Uludag <m231@tnctr.com>
2023-04-17 22:02:45 +0200

    added constexpr mktime

--- include/get_time.hpp
+++ include/get_time.hpp
@@ -26,6 +26,45 @@ inline constexpr auto check_range(int32_t value, int32_t min, int32_t max) {
     if (value < min || value > max)
         throw std::out_of_range("value is out of range!");
 }
+  
+inline constexpr auto mktime(std::tm& tm) -> std::time_t {
+    auto is_leap = [](unsigned y){
+    y += 1900;
+    return (y % 4) == 0 && ((y % 100) != 0 || (y % 400) == 0);
+};
+
+    constexpr unsigned ndays[2][12] ={
+        {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+        {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+
+    std::time_t res{0};
+    unsigned i{0};
+
+    if (tm.tm_mon > 12 ||
+        tm.tm_mon < 0 ||
+        tm.tm_mday > 31 ||
+        tm.tm_min > 60 ||
+        tm.tm_sec > 60 ||
+        tm.tm_hour > 24) {
+        /* invalid tm structure */
+        return 0;
+    }
+    
+    for (i = 70; i < tm.tm_year; ++i)
+        res += is_leap(i) ? 366 : 365;
+    
+    for (i = 0; i < tm.tm_mon; ++i)
+        res += ndays[is_leap(tm.tm_year)][i];
+    res += tm.tm_mday - 1;
+    res *= 24;
+    res += tm.tm_hour;
+    res *= 60;
+    res += tm.tm_min;
+    res *= 60;
+    res += tm.tm_sec;
+
+    return res;
+}
 
 template <typename T>
 std::enable_if_t<std::is_same_v<std::tm, T>, T> constexpr get_time(
@@ -178,7 +217,7 @@ std::enable_if_t<std::is_same_v<std::chrono::system_clock::time_point, T>,
                  T> constexpr get_time(std::string_view format,
                                              std::string_view date_str) {
     auto tm = get_time<std::tm>(format, date_str);
-    auto time_t = std::mktime(&tm);
+    auto time_t = mgutility::mktime(tm);
     T clock = std::chrono::system_clock::from_time_t(time_t);
     return clock;
 }
