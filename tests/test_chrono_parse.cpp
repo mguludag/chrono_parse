aa8635dea59182d32dc84cb5709df73fbd6328ce
M. Galib Uludag <m231@tnctr.com>
2025-09-06 10:01:38 +0000

    Refactor parsing functions to return std::error_code and simplify error handling; update tests for ISO 8601 parsing.

--- tests/test_chrono_parse.cpp
+++ tests/test_chrono_parse.cpp
@@ -2,29 +2,21 @@
 #include "doctest/doctest.h"
 #include "mgutility/chrono/parse.hpp"
 
-#if defined(__GNUC__) && !defined(__clang__)
-#define FACTOR 1000
-#else
-#define FACTOR 1
-#endif
+template <typename T>
+auto to_milliseconds(T time_point) -> std::chrono::milliseconds {
+  return std::chrono::duration_cast<std::chrono::milliseconds>(time_point.time_since_epoch());
+}
 
 TEST_CASE("testing the iso8601 parsing") {
-  CHECK(mgutility::chrono::parse("{:%FT%T}", "2023-04-30T16:22:18")
-            .time_since_epoch()
-            .count() == (1682871738000000 * FACTOR));
-  CHECK(mgutility::chrono::parse("{:%FT%T%z}", "2023-04-30T18:22:18+0200")
-            .time_since_epoch()
-            .count() == (1682871738000000 * FACTOR));
-  CHECK(mgutility::chrono::parse("{:%FT%T%z}", "2023-04-30T16:22:18-0200")
-            .time_since_epoch()
-            .count() == (1682878938000000 * FACTOR));
-  CHECK(mgutility::chrono::parse("{:%FT%T.%f}", "2023-04-30T16:22:18.500")
-            .time_since_epoch()
-            .count() == (1682871738500000 * FACTOR));
-  CHECK(
-      mgutility::chrono::parse("{:%FT%T.%f%z}", "2023-04-30T16:22:18.500+0100")
-          .time_since_epoch()
-          .count() == (1682868138500000 * FACTOR));
+  using namespace std::chrono_literals;
+
+  CHECK(to_milliseconds(mgutility::chrono::parse("{:%FT%T}", "2023-04-30T16:22:18")) == 1682871738000ms);
+  CHECK(to_milliseconds(mgutility::chrono::parse("{:%FT%T%z}", "2023-04-30T16:22:18Z")) == 1682871738000ms);
+  CHECK(to_milliseconds(mgutility::chrono::parse("{:%FT%T%z}", "2023-04-30T18:22:18+0200")) == 1682871738000ms);
+  CHECK(to_milliseconds(mgutility::chrono::parse("{:%FT%T%z}", "2023-04-30T16:22:18-0200")) == 1682878938000ms);
+  CHECK(to_milliseconds(mgutility::chrono::parse("{:%FT%T.%f}", "2023-04-30T16:22:18.500")) == 1682871738500ms);
+  CHECK(to_milliseconds(mgutility::chrono::parse("{:%FT%T.%f%z}", "2023-04-30T16:22:18.500+0100")) == 1682868138500ms);
+
   REQUIRE_THROWS(mgutility::chrono::parse("{:%FT%T", "2023-04-30T16:22:18"));
   REQUIRE_THROWS(mgutility::chrono::parse("%FT%T}", "2023-04-30T16:22:18"));
   REQUIRE_THROWS(mgutility::chrono::parse("{%F %T}", "2023-04-30T16:22:18"));
